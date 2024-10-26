09a85ae3709613ec13db9e36d2e3c4a7497e6017
Jay Prasad <jbp127@scarletmail.rutgers.edu>
2024-10-26 12:16:47 -0400

    data

--- src/foodai.py
+++ src/foodai.py
@@ -1,3 +1,17 @@
+import pandas as pd
+
 def food_ai(preferences, **kwargs):
 
+
+
     pass
+
+def take_data(csv):
+
+    df = pd.read_csv(csv)
+
+    df_smaller = df.tail(10000)
+
+    df_smaller.to_csv("tail_10000.csv", index=False)
+
+take_data("src/full_dataset.csv")
