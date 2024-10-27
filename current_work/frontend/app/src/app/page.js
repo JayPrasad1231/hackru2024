1fd69a2e238f57a561d8c511cef0aac2b96af3a0
Jay Prasad <jbp127@scarletmail.rutgers.edu>
2024-10-27 11:52:19 -0400

    oh my days we're done!

--- current_work/frontend/app/src/app/page.js
+++ current_work/frontend/app/src/app/page.js
@@ -4,35 +4,14 @@
 import React, { useEffect, useState } from 'react';
 
 const LandingPage = () => {
-  const [text, setText] = useState('');
-  const fullText = 'Weelcome to Gambling Guru!';
-  const typingDelay = 60; // delay between typing letters
-
-  useEffect(() => {
-    let index = 0;
-
-    const typeText = () => {
-      if (index < fullText.length) {
-        setText((prev) => prev + fullText.charAt(index));
-        index++;
-        setTimeout(typeText, typingDelay);
-      }
-    };
-
-    typeText();
-
-    // Cleanup function to avoid memory leaks
-    return () => {
-      setText(''); // reset text on unmount
-    };
-  }, []);
+  const fullText = 'Welcome to Gambling Guru!';
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-900">
-      <h1 className="text-9xl font-bold text-white">
-        {text.substring(0, text.indexOf("Gambling Guru!"))}
+      <h1 className="text-8xl font-bold text-white">
+        {fullText.substring(0, fullText.indexOf("Gambling Guru!"))}
         <span className="text-red-400">
-          {text.substring(text.indexOf("Gambling Guru!"))}
+          {fullText.substring(fullText.indexOf("Gambling Guru!"))}
         </span>
       </h1>
     </div>
