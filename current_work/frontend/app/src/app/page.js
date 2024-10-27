d4a6f8ef342284b61296e6724801155711d0884d
Christian DeGuzman <ckdeguz@gmail.com>
2024-10-27 00:46:07 -0400

    landing page

--- current_work/frontend/app/src/app/page.js
+++ current_work/frontend/app/src/app/page.js
@@ -3,20 +3,30 @@
 import Image from "next/image";
 import { useRouter } from 'next/navigation'
 
+const buttonData = [
+  { label: "Kelley Criterion", path: "/test" },
+  { label: "EV Betting Tips", path: "/test" },
+  { label: "Matched Betting Tips", path: "/test" },
+  { label: "Arbitrage Betting", path: "/test" },
+  { label: "Low Hold Betting", path: "/test" },
+  { label: "VIP Program", path: "/test" },
+  { label: "Authors", path: "/test" },
+];
 
 export default function Home() {
   const router = useRouter()
   
   return (
-    <div class="flex justify-between bg-violet-500 p-6">
-      <p class="text-red-300">YOOOO</p>
-      <p>BRUHH</p>
-      <p>emacs</p>
-
-      <button class="text-violet-900 bg-green-400 p-3" onClick={() => router.push('/test')}>
-        PUSH ME BRUH
-      </button>
-
+    <div className="flex justify-left bg-stone-900 p-6 gap-4 shadow-2xl">
+      {buttonData.map((button, index) => (
+        <button
+          key={index}
+          className="text-stone-900 bg-red-400 p-3 rounded-lg"
+          onClick={() => router.push(button.path)}
+        >
+          {button.label}
+        </button>
+      ))}
     </div>
   );
 }
